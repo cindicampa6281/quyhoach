33641312d2eee73e43139e1a2bbfbfddb0e76673
nguyenvanchien1999prohehe <164542987+nguyenvanchien1999prohehe@users.noreply.github.com>
2025-08-06 15:38:55 +0700

    sdfsdgsd

--- bandonhiet_tinh/Models/LandAuctionId.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, func, Text
-from Database.Database import *
-
-
-class LandAuctionId(Base):
-    __tablename__ = 'LandAuctionId'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    url = Column(Text, nullable=False)
